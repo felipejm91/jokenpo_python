fa29042c1808e94b937a20344976245fe747205a
felipejm91 <flmeneguetti@yahoo.com.br>
2022-04-18 15:35:20 -0300

    criando repositorio

---
+++ jokenpo.py
@@ -0,0 +1,49 @@
+#Crie um programa que faça o computador jogar Jokenpô com você
+
+from random import randint
+from time import sleep
+
+itens = ('pedra', 'papel', 'tesoura')
+
+print("Escolha sua jogada")
+print("{}0 - PEDRA{}" .format("\033[1;36m", "\033[m"))
+print("{}1 - PAPEL{}" .format("\033[1;35m", "\033[m"))
+print("{}2 - TESOURA{}" .format("\033[1;34m", "\033[m"))
+op = int(input("OPÇÃO: "))
+
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PÔ')
+sleep(1)
+
+jogadapc = randint(0, 2)
+
+print('-=' * 10)
+print("COMPUTADOR = {}" .format(itens[jogadapc]))
+print("JOGADOR = {}" .format(itens[op]))
+print('-=' * 10)
+
+if(jogadapc == 0):
+    if(op == 0):
+        print("{}EMPATE{}" .format("\033[1;33m", "\033[m"))        
+    elif(op == 1):               
+        print("{}JOGADOR VENCEU{}" .format("\033[1;32m", "\033[m"))
+    elif(op == 2):        
+        print("{}JOGADOR PERDEU{}" .format("\033[1;31m", "\033[m"))
+elif(jogadapc == 1):
+    if(op == 0):            
+        print("{}JOGADOR PERDEU{}" .format("\033[1;31m", "\033[m"))
+    elif(op == 1):
+        print("{}EMPATE{}" .format("\033[1;33m", "\033[m"))
+    elif(op ==2):
+        print("{}JOGADOR VENCEU{}" .format("\033[1;32m", "\033[m"))
+elif(jogadapc == 2):                
+    if(op == 0):
+        print("{}JOGADOR VENCEU{}" .format("\033[1;32m", "\033[m"))                   
+    elif(op == 1):
+        print("{}JOGADOR PERDEU{}" .format("\033[1;31m", "\033[m"))
+    elif(op ==2):
+        print("{}EMPATE{}" .format("\033[1;33m", "\033[m"))
+                

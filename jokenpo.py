4791ba80d2e0ca010280263b5ac5055808b00a7c
felipejm91 <flmeneguetti@yahoo.com.br>
2022-04-26 16:47:22 -0300

    Adição dockerfile

--- jokenpo.py
+++ jokenpo.py
@@ -5,45 +5,54 @@ from time import sleep
 
 itens = ('pedra', 'papel', 'tesoura')
 
-print("Escolha sua jogada")
-print("{}0 - PEDRA{}" .format("\033[1;36m", "\033[m"))
-print("{}1 - PAPEL{}" .format("\033[1;35m", "\033[m"))
-print("{}2 - TESOURA{}" .format("\033[1;34m", "\033[m"))
-op = int(input("OPÇÃO: "))
+op = 0
 
-print('JO')
-sleep(1)
-print('KEN')
-sleep(1)
-print('PÔ')
-sleep(1)
+while(op != 5):
+    print
+    print("\n\nEscolha sua jogada")
+    print("{}0 - PEDRA{}" .format("\033[1;36m", "\033[m"))
+    print("{}1 - PAPEL{}" .format("\033[1;35m", "\033[m"))
+    print("{}2 - TESOURA{}" .format("\033[1;34m", "\033[m"))
+    print("{}5 - SAIR DO PROGRAMA{}" .format("\033[1;31m", "\033[m"))
+    op = int(input("OPÇÃO: "))
 
-jogadapc = randint(0, 2)
+    if (op == 5):
+        print ("\nAté a próxima!")
+        break
+    else:
+        print('JO')
+        sleep(1)
+        print('KEN')
+        sleep(1)
+        print('PÔ')
+        sleep(1)
 
-print('-=' * 10)
-print("COMPUTADOR = {}" .format(itens[jogadapc]))
-print("JOGADOR = {}" .format(itens[op]))
-print('-=' * 10)
+        jogadapc = randint(0, 2)
 
-if(jogadapc == 0):
-    if(op == 0):
-        print("{}EMPATE{}" .format("\033[1;33m", "\033[m"))        
-    elif(op == 1):               
-        print("{}JOGADOR VENCEU{}" .format("\033[1;32m", "\033[m"))
-    elif(op == 2):        
-        print("{}JOGADOR PERDEU{}" .format("\033[1;31m", "\033[m"))
-elif(jogadapc == 1):
-    if(op == 0):            
-        print("{}JOGADOR PERDEU{}" .format("\033[1;31m", "\033[m"))
-    elif(op == 1):
-        print("{}EMPATE{}" .format("\033[1;33m", "\033[m"))
-    elif(op ==2):
-        print("{}JOGADOR VENCEU{}" .format("\033[1;32m", "\033[m"))
-elif(jogadapc == 2):                
-    if(op == 0):
-        print("{}JOGADOR VENCEU{}" .format("\033[1;32m", "\033[m"))                   
-    elif(op == 1):
-        print("{}JOGADOR PERDEU{}" .format("\033[1;31m", "\033[m"))
-    elif(op ==2):
-        print("{}EMPATE{}" .format("\033[1;33m", "\033[m"))
-                
+        print('-=' * 10)
+        print("COMPUTADOR = {}" .format(itens[jogadapc]))
+        print("JOGADOR = {}" .format(itens[op]))
+        print('-=' * 10)
+
+        if(jogadapc == 0):
+            if(op == 0):
+                print("{}EMPATE{}" .format("\033[1;33m", "\033[m"))        
+            elif(op == 1):               
+                print("{}JOGADOR VENCEU{}" .format("\033[1;32m", "\033[m"))
+            elif(op == 2):        
+                print("{}JOGADOR PERDEU{}" .format("\033[1;31m", "\033[m"))
+        elif(jogadapc == 1):
+            if(op == 0):            
+                print("{}JOGADOR PERDEU{}" .format("\033[1;31m", "\033[m"))
+            elif(op == 1):
+                print("{}EMPATE{}" .format("\033[1;33m", "\033[m"))
+            elif(op ==2):
+                print("{}JOGADOR VENCEU{}" .format("\033[1;32m", "\033[m"))
+        elif(jogadapc == 2):                
+            if(op == 0):
+                print("{}JOGADOR VENCEU{}" .format("\033[1;32m", "\033[m"))                   
+            elif(op == 1):
+                print("{}JOGADOR PERDEU{}" .format("\033[1;31m", "\033[m"))
+            elif(op ==2):
+                print("{}EMPATE{}" .format("\033[1;33m", "\033[m"))
+                        
